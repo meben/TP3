4d43734d8c6c3d3edb4a3a7462504c866d3f390d
Mehdi Ben Abdelkader <mehdi.benabd@gmail.com>
2013-10-04 12:55:58 +0100

    first commit

---
+++ src/com/mehdi/tp3/MainActivity.java
@@ -0,0 +1,48 @@
+package com.mehdi.tp3;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	
+	Button btn;
+	EditText champs;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        btn = (Button)findViewById(R.id.button1);
+        champs = (EditText)findViewById(R.id.editText1);
+        btn.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		Uri x = Uri.parse("tel:"+champs.getText().toString());
+		Intent i = new Intent(Intent.ACTION_CALL, x);
+		startActivity(i);
+		
+		
+	}
+    
+}
